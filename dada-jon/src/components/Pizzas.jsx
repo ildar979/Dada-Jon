c237cf8c7c5b5cf241a2f145f0d9de58c11edbf9
Ildar <ildar9@list.ru>
2022-10-02 17:25:03 +0300

    sent a request to DB at mockapi and get listPizza

--- dada-jon/src/components/Pizzas.jsx
+++ dada-jon/src/components/Pizzas.jsx
@@ -1,7 +1,17 @@
-import listPizza from '../assets/pizzas.json';
+import React, { useEffect, useState } from 'react';
 import Pizza from './Pizza';
 
 const Pizzas = () => {
+  const URL = 'https://633980e0383946bc7ff42d80.mockapi.io/items';
+
+  const [listPizza, setListPizza] = useState([]);
+
+  useEffect(() => {
+    fetch(URL)
+      .then((res) => res.json())
+      .then((data) => setListPizza(data));
+  }, []);
+
   return (
     <>
       {listPizza?.map((pizza) => (
